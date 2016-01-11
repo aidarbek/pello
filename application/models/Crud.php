3c13206859277356ba238983af1ac82c5ce5023a
aidarbek <aidarbek98@gmail.com>
2016-01-11 22:39:39 +0600

    Initial commit

---
+++ application/models/Crud.php
@@ -0,0 +1,14 @@
+<?php
+class Crud extends CI_Model {
+
+    public $table = "";
+    public $id = "";
+
+    public function __construct()
+    {
+        // Call the CI_Model constructor
+        parent::__construct();
+    }
+
+    
+}
